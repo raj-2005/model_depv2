4a28040232ace3058ee9e4a67bb23055cbfea2fa
raj-2005 <154008511+raj-2005@users.noreply.github.com>
2024-09-29 14:19:56 +0530

    Add files via upload

---
+++ application_1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+# STREAMLIT 
+
+
+# In[13]:
+
+
+pip install streamlit
+
+
+# In[14]:
+
+
+import streamlit as st
+
+
+# In[15]:
+
+
+st.title("My first Streamlit App")
+st.write("Hello , World")
+
+
+# In[16]:
+
+
+number =st.number_input("insert a number : ")
+st.write(f"entered : {number}")
+
+
+# In[17]:
+
+
+
+
+
+# In[18]:
+
+
+name=st.text_input("enter your name: ")
+st.write(f"your entered name is {name}")
+
+
+# In[19]:
+
+
+age=st.slider("your age: ",0,100,25)
+st.write(f"your entered age is {age}")
+
+
+# In[ ]:
+
+
+
+
